4b8ffaee13292a085d717aad17ea96b5b51e7eb2
sadeghbiglar <sbmail555@gmail.com>
2024-11-09 19:19:50 +0330

    upload project

---
+++ CmdClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+
+class Client
+{
+    static void Main(string[] args)
+    {
+        using (TcpClient client = new TcpClient("localhost", 5002))
+        using (NetworkStream stream = client.GetStream())
+        using (StreamWriter writer = new StreamWriter(stream) { AutoFlush = true })
+        using (StreamReader reader = new StreamReader(stream))
+        {
+            Console.Write("Enter command: ");
+            string command = Console.ReadLine();
+            writer.WriteLine(command);
+
+            string response;
+            while ((response = reader.ReadLine()) != "END_OF_MESSAGE")
+            {
+                Console.WriteLine(response);
+               
+            }
+            Console.ReadKey();
+        }
+    }
+}
